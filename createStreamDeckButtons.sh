4b669e0036c5bb340a4bf4ea79e854361e20fad0
Paul Bono <paulbono@users.noreply.github.com>
2022-06-06 20:40:30 -0500

    Various updates: Update zoom command Move pan_tilt_zoom_focus Add goodnight preset Add query all Add advance scene list Add baptism preset
    Signed-off-by: Paul Bono <paulbono@users.noreply.github.com>

--- createStreamDeckButtons.sh
+++ createStreamDeckButtons.sh
@@ -1,11 +1,12 @@
-#!/bin/bash //-x
+#!/bin/bash 
+#-x
 
 function createImage(){
    text="$1"
    name="$2"
    color="$3"
    # Want an image of 288x288, border of 10x10 adds 20 to height and width
-   magick convert -size "268x268" -background $color -fill black \
+   magick convert -size "268x268" -background $color -fill white \
     -border "10x10" -bordercolor $color \
 	-gravity Center caption:"$text" \
 	-flatten "StreamDeckButtons/$name"
@@ -13,22 +14,23 @@ function createImage(){
 
 mkdir -p StreamDeckButtons
 
-createImage "Main Worship Center" "MWC.png" "lightblue" &
-createImage "Main Worship Center PNP" "MWCP.png" "lightblue" &
-createImage "Main Pulpit Center" "MPC.png" "lightblue" &
-createImage "Main Pulpit Center PNP" "MPCP.png" "lightblue" &
-createImage "Main Sermon Center" "MSC.png" "lightblue" &
-createImage "Main Sermon Center PNP" "MSCP.png" "lightblue" &
-createImage "Main Altar Center" "MAC.png" "lightblue" &
-createImage "Main Altar Center PNP" "MACP.png" "lightblue" &
-createImage "Main Wide" "MW.png" "lightblue" &
+createImage "Main Worship Center" "MWC.png" "blue" &
+createImage "Main Worship Center PNP" "MWCP.png" "blue" &
+createImage "Main Pulpit Center" "MPC.png" "blue" &
+createImage "Main Pulpit Center PNP" "MPCP.png" "blue" &
+createImage "Main Sermon Center" "MSC.png" "blue" &
+createImage "Main Sermon Center PNP" "MSCP.png" "blue" &
+createImage "Main Altar Center" "MAC.png" "blue" &
+createImage "Main Altar Center PNP" "MACP.png" "blue" &
+createImage "Main Wide" "MW.png" "blue" &
+createImage "Main Baptism" "MB.png" "blue" &
 
-createImage "Alt Worship Center" "AWC.png" "lightgreen" &
-createImage "Alt Worship Center PNP" "AWCP.png" "lightgreen" &
-createImage "Alt Pulpit Center" "APC.png" "lightgreen" &
-createImage "Alt Pulpit Center PNP" "APCP.png" "lightgreen" &
-createImage "Alt Sermon Center" "ASC.png" "lightgreen" &
-createImage "Alt Sermon Center PNP" "ASCP.png" "lightgreen" &
-createImage "Alt Altar Center" "AAC.png" "lightgreen" &
-createImage "Alt Altar Center PNP" "AACP.png" "lightgreen" &
-createImage "Alt Wide" "AW.png" "lightgreen"
+createImage "Alt Worship Center" "AWC.png" "green" &
+createImage "Alt Worship Center PNP" "AWCP.png" "green" &
+createImage "Alt Pulpit Center" "APC.png" "green" &
+createImage "Alt Pulpit Center PNP" "APCP.png" "green" &
+createImage "Alt Sermon Center" "ASC.png" "green" &
+createImage "Alt Sermon Center PNP" "ASCP.png" "green" &
+createImage "Alt Altar Center" "AAC.png" "green" &
+createImage "Alt Altar Center PNP" "AACP.png" "green" &
+createImage "Alt Wide" "AW.png" "green"
