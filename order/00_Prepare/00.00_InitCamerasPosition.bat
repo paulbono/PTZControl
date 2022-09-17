8a68eeb09451132243310121ece24d5f6f167208
unknown <User@W10-D-24.celc.christpewaukee.org>
2022-09-17 17:53:41 -0500

    More stuffs

--- order/00_Prepare/00.00_InitCamerasPosition.bat
+++ order/00_Prepare/00.00_InitCamerasPosition.bat
@@ -1,6 +1,12 @@
 REM NEEDS TO BE UPDATED
 pushd C:\Users\User\Desktop\PTZControl
 	REM C:\Users\User\AppData\Local\Programs\Python\Python310\python.exe .\ptz_cameras.py --main --preset "worship_center"
-	REM REM button 1/4 is camera 6
-	REM curl http://127.0.0.1:8888/press/bank/1/4
+	REM button 1/4 is camera 6
+	curl http://127.0.0.1:8888/press/bank/1/4
+	REM AUTO
+	curl http://127.0.0.1:8888/press/bank/1/2
+	REM 1:00
+	curl http://127.0.0.1:8888/press/bank/2/26
+	REM 1:00
+	curl http://127.0.0.1:8888/press/bank/2/16
 popd 
