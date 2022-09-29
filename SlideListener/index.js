ad7113af759c59f3a00f50a4ba6c4808d5e3d483
Paul Bono <paulbono@users.noreply.github.com>
2022-09-28 19:05:37 -0500

    tab to space
    Signed-off-by: Paul Bono <paulbono@users.noreply.github.com>

--- SlideListener/index.js
+++ SlideListener/index.js
@@ -12,17 +12,17 @@ let ptzData = JSON.parse(ptzRawData);
 
 // Press a button
 function pressBitCompanionButton(page, button) {
-	return new Promise(function (resolve, reject) {
-		let bitCompanionOptions = {
-			hostname: "127.0.0.1",
-			port: "8888",
-			path: "/press/bank" + page + "/" + button,
-			method: "GET"
-		};
-		http.request(bitCompanionOptions, res => {
-			resolve(console.log(res));
-		});
-	});
+    return new Promise(function (resolve, reject) {
+        let bitCompanionOptions = {
+            hostname: "127.0.0.1",
+            port: "8888",
+            path: "/press/bank" + page + "/" + button,
+            method: "GET"
+        };
+        http.request(bitCompanionOptions, res => {
+            resolve(console.log(res));
+        });
+    });
 }
 
 const ONE_SECOND_IN_MS = 1000;
@@ -41,269 +41,269 @@ let setSessionTags = [];
 
 // Assuming tag will always exist in post data
 app.post('/slide', async function (req, res) {
-	console.log(req.body);
-	//res.json(req.body);
+    console.log(req.body);
+    //res.json(req.body);
 
-	// Doing this so we don't trigger the same tag twice in a given "service" since there is state involved here
-	if (req.body.tag == "[Start]") {
-		setSessionTags = [];
-	} else if (setSessionTags.includes(req.body.tag)) {
-		res.send('');
-		return;
-	} else {
-		setSessionTags.push(req.body.tag);
-	}
+    // Doing this so we don't trigger the same tag twice in a given "service" since there is state involved here
+    if (req.body.tag == "[Start]") {
+        setSessionTags = [];
+    } else if (setSessionTags.includes(req.body.tag)) {
+        res.send('');
+        return;
+    } else {
+        setSessionTags.push(req.body.tag);
+    }
 
-	switch(req.body.tag)
-	{
-		case "[Worship Hymn]":
-			// Camera 7
-			await pressBitCompanionButton(1, 5);
+    switch(req.body.tag)
+    {
+        case "[Worship Hymn]":
+            // Camera 7
+            await pressBitCompanionButton(1, 5);
 
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Call To Worship]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "worship_center_pnp"}, "main");
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Call To Worship]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "worship_center_pnp"}, "main");
 
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
 
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
 
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Old Testment]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "pulpit_center_pnp"}, "main");
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Old Testment]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "pulpit_center_pnp"}, "main");
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
 
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Sermon Hymn]":
-			// Camera 7
-			await pressBitCompanionButton(1, 5);
-			// Press Mac Overlay
-			await pressBitCompanionButton(1, 10);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Sermon Hymn]":
+            // Camera 7
+            await pressBitCompanionButton(1, 5);
+            // Press Mac Overlay
+            await pressBitCompanionButton(1, 10);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
 
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Sermon Text]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "sermon_center_pnp"}, "main");
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Sermon Theme]":
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "sermon_center"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Sermon Text]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "sermon_center_pnp"}, "main");
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Sermon Theme]":
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "sermon_center"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
 
-			// Wait 5 minutes
-			await sleep(FIVE_MINUTES_IN_MS);
+            // Wait 5 minutes
+            await sleep(FIVE_MINUTES_IN_MS);
 
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "sermon_center"}, "main");
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "sermon_center"}, "main");
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
 
-			// Wait 5 minutes
-			await sleep(FIVE_MINUTES_IN_MS);
+            // Wait 5 minutes
+            await sleep(FIVE_MINUTES_IN_MS);
 
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "sermon_center"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "sermon_center"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
 
-			break;
-		case "[Creed]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "pulpit_center_pnp"}, "main");
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Prayer]":
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "altar_center"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Lords Prayer]":
-			// Camera 7
-			await pressBitCompanionButton(1, 5);
-			// Press Mac Overlay
-			await pressBitCompanionButton(1, 10);
+            break;
+        case "[Creed]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "pulpit_center_pnp"}, "main");
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Prayer]":
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "altar_center"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Lords Prayer]":
+            // Camera 7
+            await pressBitCompanionButton(1, 5);
+            // Press Mac Overlay
+            await pressBitCompanionButton(1, 10);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Institution]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "altar_center"}, "main");
-			// Press Mac Overlay
-			await pressBitCompanionButton(1, 10);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Institution]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "altar_center"}, "main");
+            // Press Mac Overlay
+            await pressBitCompanionButton(1, 10);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Distribution]":
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "wide"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[End of Distribution]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "altar_center_pnp"}, "main");
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Closing Hymn]":
-			// Camera 7
-			await pressBitCompanionButton(1, 5);
-			// Press Mac Overlay
-			await pressBitCompanionButton(1, 10);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Distribution]":
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "wide"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[End of Distribution]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "altar_center_pnp"}, "main");
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Closing Hymn]":
+            // Camera 7
+            await pressBitCompanionButton(1, 5);
+            // Press Mac Overlay
+            await pressBitCompanionButton(1, 10);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Silent prayer]":
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "wide"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Announcements]":
-			// Point Camera 5
-			camera.send_commands(ptzData, {"preset": "worship_center"}, "main");
-			// Press PNP Overlay
-			await pressBitCompanionButton(1, 11);
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
-			// Camera 5
-			await pressBitCompanionButton(1, 3);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[After Service Video]":
-			// Camera 7
-			await pressBitCompanionButton(1, 5);
-			// Press Mac Overlay
-			await pressBitCompanionButton(1, 10);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Silent prayer]":
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "wide"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Announcements]":
+            // Point Camera 5
+            camera.send_commands(ptzData, {"preset": "worship_center"}, "main");
+            // Press PNP Overlay
+            await pressBitCompanionButton(1, 11);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
+            // Camera 5
+            await pressBitCompanionButton(1, 3);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[After Service Video]":
+            // Camera 7
+            await pressBitCompanionButton(1, 5);
+            // Press Mac Overlay
+            await pressBitCompanionButton(1, 10);
 
-			// Press Toggle A
-			//await pressBitCompanionButton(1, 30);
-			// Press Toggle B
-			//await pressBitCompanionButton(1, 31);
+            // Press Toggle A
+            //await pressBitCompanionButton(1, 30);
+            // Press Toggle B
+            //await pressBitCompanionButton(1, 31);
 
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			break;
-		case "[Thank You For Worshiping]":
-			// Wait 10 Seconds
-			await sleep(TEN_SECONDS_IN_MS);
-			// Point Camera 6
-			camera.send_commands(ptzData, {"preset": "wide"}, "alt");
-			// Camera 6
-			await pressBitCompanionButton(1, 4);
-			// Press Auto
-			await pressBitCompanionButton(1, 2);
-			// Wait 15 Seconds
-			await sleep(FIFTEEN_SECONDS_IN_MS);
-			// TODO Copyright
-			// Wait 15 Seconds
-			await sleep(FIFTEEN_SECONDS_IN_MS);
-			// TODO Stop Stream
-			break;
-		default:
-			console.log("Don't know this tag");
-	}
-	res.send('');
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            break;
+        case "[Thank You For Worshiping]":
+            // Wait 10 Seconds
+            await sleep(TEN_SECONDS_IN_MS);
+            // Point Camera 6
+            camera.send_commands(ptzData, {"preset": "wide"}, "alt");
+            // Camera 6
+            await pressBitCompanionButton(1, 4);
+            // Press Auto
+            await pressBitCompanionButton(1, 2);
+            // Wait 15 Seconds
+            await sleep(FIFTEEN_SECONDS_IN_MS);
+            // TODO Copyright
+            // Wait 15 Seconds
+            await sleep(FIFTEEN_SECONDS_IN_MS);
+            // TODO Stop Stream
+            break;
+        default:
+            console.log("Don't know this tag");
+    }
+    res.send('');
 })
 
 app.listen(port, () => {});
