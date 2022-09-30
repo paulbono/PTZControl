ef91bd18cf58b5c0ae3ff600fcd8cac518e59a6a
Paul Bono <paulbono@users.noreply.github.com>
2022-09-29 22:30:44 -0500

    Make windows friendly
    Signed-off-by: Paul Bono <paulbono@users.noreply.github.com>

--- order/bas/startStream.bat
+++ order/bas/startStream.bat
@@ -1,4 +1,4 @@
 @echo off
 
 echo Begin Program
-curl -i -X POST -H 'Content-Type: application/json' -d '{"tag": "[Begin Program]"}' localhost:3000/slide
+curl -i -X POST -H "Content-Type: application/json" -d "{'tag': '[Begin Program]'}" 127.0.0.1:3000/slide
